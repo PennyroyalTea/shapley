8814647ce895bd77b8b9dde71a0420df5f1e23be
Boris Starkov <boris.starkow@gmail.com>
2020-11-24 19:08:55 +0300

    reimplement shapley as a function

--- shapley.py
+++ shapley.py
@@ -1,38 +1,30 @@
-from torch.utils.data import Dataset
-
 import random
 
-class Shapley:
+def monte_carlo(train_set, val_set, model, ITER=1000, perf_tolerance=0.1):
 
-    ITER = 1000  # num of iterations before convergence
-    dataset = None # dataset to take training points from
-    perf_tolerance = 0.1 # some constant need to be researched later
-    shapley_vals = []
+    shapley_vals = [0] * len(train_set)
+
+    ################################
+    permutation = list(range(len(train_set))) # [0, 1, ..., len(train_set)]
+
+    V_D = train_on_subset(permutation).eval()  # score of fully-trained model
+    V_current = train_on_subset([]).eval()  # score of untrained model
+    V_prev = V_current
+
+    for _ in range(ITER):  # convergence criteria
+        random.shuffle(permutation)
+        for i in range(len(dataset)):
+            V_prev = V_current
+            if abs(V_D - V_current) > perf_tolerance:
+                V_current = train_on_subset(permutation[:i]).eval()  # score on first i datapoints
+
+            shapley_vals[permutation[i]] = ((t - 1) * shapley_vals[permutation[i]] + (
+                        V_current - V_prev)) / t  # update shapley values
+    ################################
 
 
-    def __init__(self, dataset):
-        """
-        Constructor which takes the dataset
-        :param dataset: Object of class torch.utils.data.Dataset.
-        """
-        self.dataset = dataset
-        self.shapley_vals = [0] * len(dataset)
+    return shapley_vals
 
-    def calculate_shapley_values(self):
-        """
-        calculates shapley values using Monte-Carlo approach
-        """
-        permutation = list(range(len(dataset)))
-
-        V_D = train_on_subset(permutation).eval() # score of fully-trained model
-        V_current = train_on_subset([]).eval() # score of untrained model
-        V_prev = V_current
-
-        for _ in range(ITER): # convergence criteria
-            random.shuffle(permutation)
-            for i in range(len(dataset)):
-                V_prev = V_current
-                if abs(V_D - V_current) > perf_tolerance:
-                    V_current = train_on_subset(permutation[:i]).eval() # score on first i datapoints
-
-                shapley_vals[permutation[i]] = ((t-1) * shapley_vals[permutation[i]] + (V_current - V_prev)) / t # update shapley values
+def gradient_descend():
+    # TODO: implement grad descend approach
+    pass
