e19bed91a57e8a6680d8cbd462b99c5b7b42b56c
Boris Starkov <boris.starkow@gmail.com>
2020-11-24 19:19:09 +0300

    connect shapley_utils to monte carlo implementation

--- shapley.py
+++ shapley.py
@@ -1,22 +1,47 @@
 import random
 
-def monte_carlo(train_set, val_set, model, ITER=1000, perf_tolerance=0.1):
+from shapley_utils import train_on_subset
+
+def monte_carlo(train_set, val_set, model_class, ITER=1000, perf_tolerance=0.1):
 
     shapley_vals = [0] * len(train_set)
-
-    ################################
     permutation = list(range(len(train_set))) # [0, 1, ..., len(train_set)]
 
-    V_D = train_on_subset(permutation).eval()  # score of fully-trained model
-    V_current = train_on_subset([]).eval()  # score of untrained model
+    # score of fully-trained model
+    V_D = train_on_subset(
+        model=model_class().cuda(),
+        train_set=train_set,
+        val_set=val_set,
+        permutation=permutation,
+        exp_id='full'
+    )
+
+    # score of untrained model
+    V_current = train_on_subset(
+        model=model_class().cuda(),
+        train_set=train_set,
+        val_set=val_set,
+        permutation=[],
+        exp_id='empty'
+    )
+
     V_prev = V_current
 
-    for _ in range(ITER):  # convergence criteria
+    exp_id = 0
+    for _ in range(ITER):  # convergence criteria ???
         random.shuffle(permutation)
         for i in range(len(dataset)):
             V_prev = V_current
             if abs(V_D - V_current) > perf_tolerance:
-                V_current = train_on_subset(permutation[:i]).eval()  # score on first i datapoints
+                # score on first i datapoints
+                V_current = train_on_subset(
+                    model=model_class().cuda(),
+                    train_set=train_set,
+                    val_set=val_set,
+                    permutation=permutation[:i],
+                    exp_id=exp_id
+                )
+                exp_id += 1
 
             shapley_vals[permutation[i]] = ((t - 1) * shapley_vals[permutation[i]] + (
                         V_current - V_prev)) / t  # update shapley values
